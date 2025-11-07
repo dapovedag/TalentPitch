6786591421e1cfe048d5072e4c2e66420eb0858c
talentpitch-master <talentpitchcolombia@gmail.com>
2025-11-07 06:55:54 -0500

    Actuaización según lanzamiento preliminar

---
+++ src/managers/userData/Challenge_495847.js
@@ -0,0 +1,88 @@
+export default {
+  challengeId: 495847,
+  challengeName: 'Tu Historia al Cine
+
+Alameda y Zamora Films México',
+  algorithms: {
+    1: {
+      videos: [
+        "https://media.talentpitch.co/resumes/128547/aws_v3__35464a9693da4307adb2c4a6c3df6494.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128973/timeline-1.mp4",
+        "https://media.talentpitch.co/resumes/128705/celebrity-pass-pitch.mp4",
+        "https://media.talentpitch.co/resumes/128237/vivir-en-pecado.MP4",
+        "https://media.talentpitch.co/resumes/132550/aws_v3__5b5386345d614ea0b22ed68bd5248df8.mp4",
+        "https://media.talentpitch.co/resumes/128943/whatsapp-video-2025-04-10-at-114837-am.mp4",
+        "https://media.talentpitch.co/resumes/128619/apocalipsis.mp4",
+        "https://media.talentpitch.co/resumes/131875/aws_v3__845bcc39468e47db9ad87b3b07ba2da2.temp.mp4",
+        "https://media.talentpitch.co/resumes/128801/pitch-los-vaca.mp4",
+        "https://media.talentpitch.co/resumes/129007/copy-bff05810-265c-40bd-bb62-9ff6ab57c206.mov",
+        "https://media.talentpitch.co/resumes/128661/nidodeserpientes-josezamora-videodepitch.mp4",
+        "https://media.talentpitch.co/resumes/128993/pitch-los-dos-soldados.mp4"
+      ]
+    },
+    2: {
+      videos: [
+        "https://media.talentpitch.co/resumes/128653/video-talentpitch.mp4",
+        "https://media.talentpitch.co/resumes/128051/aws_v3__7f6cbbfd6dca448c81dd814ba2767b4a.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128237/vivir-en-pecado.MP4",
+        "https://media.talentpitch.co/resumes/126889/inbound8050205494604243227.mp4",
+        "https://media.talentpitch.co/resumes/128057/pitch-casamamamaria.mp4",
+        "https://media.talentpitch.co/resumes/128547/aws_v3__35464a9693da4307adb2c4a6c3df6494.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129005/aws_v3__7b675fb3d69b4251bc405b170e1d1b5d.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128277/aws_v3__cdae02dcdab14833911255ad7f51e105.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128865/aws_v3__59b90b3b7b7b483d8d276aa3adba6208.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128703/pitchsantamaya-andrespalmaburatta.mp4",
+        "https://media.talentpitch.co/resumes/128619/apocalipsis.mp4",
+        "https://media.talentpitch.co/resumes/128211/guion-green-r-vega.mp4"
+      ]
+    },
+    3: {
+      videos: [
+        "https://media.talentpitch.co/resumes/129005/aws_v3__7b675fb3d69b4251bc405b170e1d1b5d.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128277/aws_v3__cdae02dcdab14833911255ad7f51e105.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128865/aws_v3__59b90b3b7b7b483d8d276aa3adba6208.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128547/aws_v3__35464a9693da4307adb2c4a6c3df6494.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128619/apocalipsis.mp4",
+        "https://media.talentpitch.co/resumes/128007/aws_v3__1b920fe79b9f4b98ba64d29e19d7c67c.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128703/pitchsantamaya-andrespalmaburatta.mp4",
+        "https://media.talentpitch.co/resumes/128975/aws_v3__96f1a58270314814aed774427c7764d6.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128661/nidodeserpientes-josezamora-videodepitch.mp4",
+        "https://media.talentpitch.co/resumes/128973/timeline-1.mp4",
+        "https://media.talentpitch.co/resumes/128783/aws_v3__177c49c892e24989a31f43f583492e14.mp4",
+        "https://media.talentpitch.co/resumes/128831/aws_v3__8a23c49ff6f84617a98fd95d10dc8922.MOV.mp4"
+      ]
+    },
+    4: {
+      videos: [
+        "https://media.talentpitch.co/resumes/128547/aws_v3__35464a9693da4307adb2c4a6c3df6494.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128865/aws_v3__59b90b3b7b7b483d8d276aa3adba6208.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128277/aws_v3__cdae02dcdab14833911255ad7f51e105.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128619/apocalipsis.mp4",
+        "https://media.talentpitch.co/resumes/128973/timeline-1.mp4",
+        "https://media.talentpitch.co/resumes/128975/aws_v3__96f1a58270314814aed774427c7764d6.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128661/nidodeserpientes-josezamora-videodepitch.mp4",
+        "https://media.talentpitch.co/resumes/128007/aws_v3__1b920fe79b9f4b98ba64d29e19d7c67c.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128703/pitchsantamaya-andrespalmaburatta.mp4",
+        "https://media.talentpitch.co/resumes/129003/aws_v3__965700f63310427c9fecaed2082a02e9.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128947/the-man-who-hates-the-doors-pitch.mp4",
+        "https://media.talentpitch.co/resumes/128981/aws_v3__6084bbd7fb5048408c8122ec994f7eec.MOV.mp4"
+      ]
+    },
+    5: {
+      videos: [
+        "https://media.talentpitch.co/resumes/128981/aws_v3__6084bbd7fb5048408c8122ec994f7eec.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128333/aws_v3__32cbaf79aee14123989947878f951c12.temp.mp4",
+        "https://media.talentpitch.co/resumes/128007/aws_v3__1b920fe79b9f4b98ba64d29e19d7c67c.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128051/aws_v3__7f6cbbfd6dca448c81dd814ba2767b4a.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128011/aws_v3__5d00483f81ae401fa9a9bf8559d96215.mp4",
+        "https://media.talentpitch.co/resumes/128819/tina-la-loca.mp4",
+        "https://media.talentpitch.co/resumes/128337/aws_v3__082d61003cef44c6b0cc391c501b9bec.temp.mp4",
+        "https://media.talentpitch.co/resumes/128843/aws_v3__1d049fe37cde4b058bae6c239788e780.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128703/pitchsantamaya-andrespalmaburatta.mp4",
+        "https://media.talentpitch.co/resumes/128975/aws_v3__96f1a58270314814aed774427c7764d6.MOV.mp4",
+        "https://media.talentpitch.co/resumes/128385/aws_v3__3084d3c680924437a77d5ef7e34bcd1b.mp4",
+        "https://media.talentpitch.co/resumes/128057/pitch-casamamamaria.mp4"
+      ]
+    }
+  }
+};
--- src/managers/userData/Challenge_495897.js
+++ src/managers/userData/Challenge_495897.js
@@ -6,18 +6,32 @@ export default {
       videos: [
         "https://media.talentpitch.co/resumes/129883/video-conductor-tv.mp4",
         "https://media.talentpitch.co/resumes/129579/img-1127.mov",
+        "https://media.talentpitch.co/resumes/130105/petal-20250523-234703.mp4",
         "https://media.talentpitch.co/resumes/129921/fullsizerender.MOV",
         "https://media.talentpitch.co/resumes/129545/aws_v3__c688627298a34995bb8ec27395f985d2.MOV.mp4",
-        "https://media.talentpitch.co/resumes/129791/copy-d5eb7b5d-c182-42ba-9274-179b29db23c9.mov"
+        "https://media.talentpitch.co/resumes/129875/666135e9-266b-4298-a04c-555bd1abacf6.mov",
+        "https://media.talentpitch.co/resumes/129791/copy-d5eb7b5d-c182-42ba-9274-179b29db23c9.mov",
+        "https://media.talentpitch.co/resumes/129775/copy-e72f4221-ec98-4fd5-9b33-fbd8c274a7ea.mov",
+        "https://media.talentpitch.co/resumes/129763/petal-20250512-205802.mp4",
+        "https://media.talentpitch.co/resumes/129571/aws_v3__bf9d330e19d543218fcae4a17fab4b32.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129989/aws_v3__0305d779a1ff41fb94d2d2a624a499a9.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129581/copy-85a9716d-fcaa-41c9-9b82-b6131a7afad8.mov"
       ]
     },
     2: {
       videos: [
         "https://media.talentpitch.co/resumes/129791/copy-d5eb7b5d-c182-42ba-9274-179b29db23c9.mov",
         "https://media.talentpitch.co/resumes/129875/666135e9-266b-4298-a04c-555bd1abacf6.mov",
-        "https://media.talentpitch.co/resumes/129457/aws_v3__dfe0362409f0490b94562102b28a515c.MOV.mp4",
+        "https://media.talentpitch.co/resumes/130105/petal-20250523-234703.mp4",
+        "https://media.talentpitch.co/resumes/129631/video-output-e6e7091f-57ef-48a0-962d-22c74e20aa04-2.mov",
         "https://media.talentpitch.co/resumes/129763/petal-20250512-205802.mp4",
-        "https://media.talentpitch.co/resumes/129631/video-output-e6e7091f-57ef-48a0-962d-22c74e20aa04-2.mov"
+        "https://media.talentpitch.co/resumes/129849/aws_v3__351e225276c44587861da298fe6f4356.mp4",
+        "https://media.talentpitch.co/resumes/129545/aws_v3__c688627298a34995bb8ec27395f985d2.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129581/copy-85a9716d-fcaa-41c9-9b82-b6131a7afad8.mov",
+        "https://media.talentpitch.co/resumes/129519/inbound96591137275197070.mp4",
+        "https://media.talentpitch.co/resumes/129637/inbound7282354627446358676.mp4",
+        "https://media.talentpitch.co/resumes/129883/video-conductor-tv.mp4",
+        "https://media.talentpitch.co/resumes/129579/img-1127.mov"
       ]
     },
     3: {
@@ -25,26 +39,47 @@ export default {
         "https://media.talentpitch.co/resumes/129763/petal-20250512-205802.mp4",
         "https://media.talentpitch.co/resumes/129791/copy-d5eb7b5d-c182-42ba-9274-179b29db23c9.mov",
         "https://media.talentpitch.co/resumes/129457/aws_v3__dfe0362409f0490b94562102b28a515c.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129685/aws_v3__43ae130b7c5c4d8da7a12bc0033a909d.MOV.mp4",
+        "https://media.talentpitch.co/resumes/130105/petal-20250523-234703.mp4",
+        "https://media.talentpitch.co/resumes/129581/copy-85a9716d-fcaa-41c9-9b82-b6131a7afad8.mov",
+        "https://media.talentpitch.co/resumes/129257/img-1241.mov",
+        "https://media.talentpitch.co/resumes/129709/casting-sale-el-sol.mp4",
+        "https://media.talentpitch.co/resumes/129637/inbound7282354627446358676.mp4",
+        "https://media.talentpitch.co/resumes/129405/aws_v3__a41cac1e7167467485120142768d0ecf.MOV.mp4",
         "https://media.talentpitch.co/resumes/129885/aws_v3__f8631eb3b756449ab6ee82623cb35f33.MOV.mp4",
-        "https://media.talentpitch.co/resumes/129683/aws_v3__5b7a194bd7c84beca3093937d839145f.MOV.mp4"
+        "https://media.talentpitch.co/resumes/129519/inbound96591137275197070.mp4"
       ]
     },
     4: {
       videos: [
-        "https://media.talentpitch.co/resumes/129791/copy-d5eb7b5d-c182-42ba-9274-179b29db23c9.mov",
         "https://media.talentpitch.co/resumes/129763/petal-20250512-205802.mp4",
+        "https://media.talentpitch.co/resumes/129791/copy-d5eb7b5d-c182-42ba-9274-179b29db23c9.mov",
         "https://media.talentpitch.co/resumes/129457/aws_v3__dfe0362409f0490b94562102b28a515c.MOV.mp4",
-        "https://media.talentpitch.co/resumes/129885/aws_v3__f8631eb3b756449ab6ee82623cb35f33.MOV.mp4",
-        "https://media.talentpitch.co/resumes/129571/aws_v3__bf9d330e19d543218fcae4a17fab4b32.MOV.mp4"
+        "https://media.talentpitch.co/resumes/130105/petal-20250523-234703.mp4",
+        "https://media.talentpitch.co/resumes/129303/aws_v3__8ffa525ab45f44b7bb69827082400bd8.mp4",
+        "https://media.talentpitch.co/resumes/129405/aws_v3__a41cac1e7167467485120142768d0ecf.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129519/inbound96591137275197070.mp4",
+        "https://media.talentpitch.co/resumes/129455/aws_v3__ff68a11eb7be46618f673cd8b30ae715.mp4",
+        "https://media.talentpitch.co/resumes/129729/aws_v3__d3e8532a6e0c4ac3b71d4ed4d15077b1.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129391/64b081dccdfe425c87aa3f5d3195598f.mp4",
+        "https://media.talentpitch.co/resumes/129571/aws_v3__bf9d330e19d543218fcae4a17fab4b32.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129849/aws_v3__351e225276c44587861da298fe6f4356.mp4"
       ]
     },
     5: {
       videos: [
-        "https://media.talentpitch.co/resumes/129763/petal-20250512-205802.mp4",
-        "https://media.talentpitch.co/resumes/129885/aws_v3__f8631eb3b756449ab6ee82623cb35f33.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129673/aws_v3__23fea58c9e7748999c46e23697bd4ac7.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129583/64b081dccdfe425c87aa3f5d3195598f.mp4",
+        "https://media.talentpitch.co/resumes/130105/petal-20250523-234703.mp4",
+        "https://media.talentpitch.co/resumes/129921/fullsizerender.MOV",
         "https://media.talentpitch.co/resumes/130019/aws_v3__0efbdba383a94ba3aec3dc965a28cd7e.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129953/aws_v3__45a72fe9abd64d53b0890d206ac83c50.mp4",
+        "https://media.talentpitch.co/resumes/131961/aws_v3__59fd9c2cf109429c9c9f9645e882fb75.mp4",
         "https://media.talentpitch.co/resumes/129637/inbound7282354627446358676.mp4",
-        "https://media.talentpitch.co/resumes/129883/video-conductor-tv.mp4"
+        "https://media.talentpitch.co/resumes/129545/aws_v3__c688627298a34995bb8ec27395f985d2.MOV.mp4",
+        "https://media.talentpitch.co/resumes/129709/casting-sale-el-sol.mp4",
+        "https://media.talentpitch.co/resumes/129579/img-1127.mov",
+        "https://media.talentpitch.co/resumes/129791/copy-d5eb7b5d-c182-42ba-9274-179b29db23c9.mov"
       ]
     }
   }
